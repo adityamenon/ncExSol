582d8e132897ef1cb4b1826f955ce7d83c9f1526
Aditya Menon <hello@adityamenon.co>
2017-03-06 16:09:58 +0800

    Use default express.js generator code

--- app.js
+++ app.js
@@ -1,56 +1,46 @@
-/**
- * This file needs to run the setup script and
- * wait for promises to resolve and start listening on the configured port
- *
- * In case of error it should log that to the console and abort.
- *
- * Don't be intimidated by bin/www, you can do the same thing, and get rid of some of the logic
- * Any logic you deem as absolutely necessary can be kept inside a util, tested and used
- */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-const AppSetup = require('./setup');
-const Util = require('./util');
-const express = require('express');
+var app = express();
 
-new AppSetup({
-      server: express()
-    }).run().then((server) => {
-      Util.successMessage(server);
-    }).catch((error) => {
-      Util.errorMessage(error);
-    });
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-// var path = require('path');
-// var logger = require('morgan');
-//
-// var index = require('./routes/index');
-//
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-//
-// app.use(logger('dev'));
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// app.use('/', index);
-//
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-//
-// module.exports = app;
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
