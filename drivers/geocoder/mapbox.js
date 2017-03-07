12f8290e7fb19f436be40058d7155e5a852fe3b4
Aditya M P <hello@adityamenon.co>
2017-03-08 04:32:17 +0800

    Mapbox driver and its tests complete

--- drivers/geocoder/mapbox.js
+++ drivers/geocoder/mapbox.js
@@ -1,4 +1,41 @@
-// loads up the API secret from config
-// calls the urlGenerator utility to find out how to query the actual MapBox API
-// sends the API request to get the Geocoding information
-// rejects or resolves the promise according to the result received
+const _ = require('lodash');
+const axios = require('axios');
+
+/**
+ * Mapbox is a geocoder driver instance.
+ * It handles communication with the Mapbox API, and processing of the results.
+ */
+class Mapbox {
+  constructor(accessToken) {
+    if (_.isEmpty(accessToken) || ! _.isString(accessToken)) {
+      throw new Error('Mapbox Access Token not provided.')
+    }
+
+    this.accessToken = accessToken;
+  }
+
+  // Not doing any validations here, because this driver
+  // is meant to be used via the service.
+  geocodeAddress(address) {
+    return new Promise((resolve, reject) => {
+      return axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json`, {
+        params: {
+          access_token: this.accessToken
+        }
+      }).then(response => {
+        return resolve(extractCoordinates(response.data));
+      });
+    });
+  }
+}
+
+const extractCoordinates = mapboxData => {
+  let firstFeature = mapboxData.features[0]; // TODO: ideally should be filtered by highest relevance score
+
+  return {
+    latitude: `${firstFeature.center[0]}`, // typecasting to string
+    longitude: `${firstFeature.center[1]}`
+  };
+};
+
+module.exports = Mapbox;
