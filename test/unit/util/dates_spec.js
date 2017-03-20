99333c73e49d3c3355260ff3421c95dde9d88922
Aditya M P <hello@adityamenon.co>
2017-03-21 02:02:14 +0800

    Fix travis timezone issue with date tests

--- test/unit/util/dates_spec.js
+++ test/unit/util/dates_spec.js
@@ -7,8 +7,9 @@ describe('Dates', () => {
   let clock;
 
   beforeEach(() => {
-    // freezing time for the test, at 20th Mar 2017
-    clock = sinon.useFakeTimers(moment('20-03-2017', 'DD-MM-YYYY').valueOf());
+    // freezing time for the test, at
+    // Monday, March 20th 2017, 12:00:00 am UTC+00:00
+    clock = sinon.useFakeTimers(1489968000 * 1000);
   });
 
   describe('.futureDayTimestamp', () => {
@@ -17,11 +18,11 @@ describe('Dates', () => {
     });
 
     it('provides UNIX seconds for a day in the future week', () => {
-      Dates.futureDayTimestamp('Wednesday').should.equal(1490112000);
+      Dates.futureDayTimestamp('Wednesday').should.equal(1490140800);
     });
 
     it('provides UNIX seconds for the same day next week if passed day is same as today', () => {
-      Dates.futureDayTimestamp('Monday').should.equal(1490544000);
+      Dates.futureDayTimestamp('Monday').should.equal(1490572800);
     });
   });
 
--- util/dates.js
+++ util/dates.js
@@ -5,16 +5,21 @@ class Dates {
   static futureDayTimestamp(dayName) {
     dayName = _.trim(dayName);
 
+    // avoid locale problems in runtime environments
+    // possible because the application is not to be timezone aware
+    let referenceMoment = moment().utc(false),
+        referenceMomentCopy = referenceMoment.clone();
+
     // If the ISO weekday of the passed date matches
     // with the ISO weekday of today,
     // provide a timestamp 7 days from now
-    if (moment().isoWeekday(dayName).isoWeekday() === moment().isoWeekday()) {
-      return moment().add(7, 'days').unix();
+    if (referenceMomentCopy.isoWeekday(dayName).isoWeekday() === referenceMoment.isoWeekday()) {
+      return referenceMoment.add(7, 'days').unix();
     }
 
     // otherwise, use the default momentjs logic to just look for the next
     // day in the upcoming week to match the provided day name
-    return moment().isoWeekday(dayName).unix();
+    return referenceMoment.isoWeekday(dayName).unix();
   }
 }
 
